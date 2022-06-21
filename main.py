b1eddc0fb8c8fae122abb90d75fbe0184f68b020
TheBoldReaper <airstrike2003@gnail.com>
2022-06-22 03:33:05 +0800

    First Commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.videoplayer import VideoPlayer
 
 
 class ClockedWindow(Widget):
@@ -7,9 +8,17 @@ class ClockedWindow(Widget):
 
 
 class ClockedApp(App):
+    # Window Properties
+    title = "HEHE"
+    icon = r'Assets/images/ClockedLogo.png'
+
     def build(self):
-        self.icon = r'Assets/images/ClockedLogo.png'
-        return ClockedWindow()
+        player = VideoPlayer(source="Assets/images/Neva.mp4")
+        player.state = 'play'
+        player.options = {'eos': 'loop'}  # eos stands for End of String, In which this is referring what happens when
+        # the video ends
+
+        return player
 
 
 if __name__ == '__main__':
